4ab77db2b6f8fd95b5002e302156b57d6f89a356
Savindu Dilshan <savindudilshan124@gmail.com>
2025-07-03 08:45:09 +0530

    Create kalman_filter.hpp

---
+++ include/kalman_filter.hpp
@@ -0,0 +1,28 @@
+// kalman_filter.hpp - Extended Kalman Filter for dead reckoning
+
+#pragma once
+#include <Eigen/Dense>
+
+struct Pose2D {
+    float x;
+    float y;
+    float theta;
+};
+
+class KalmanFilter {
+public:
+    KalmanFilter(float dt);
+    void predict(float linear_velocity, float angular_velocity);
+    void updateFromYaw(float measured_yaw);
+    Pose2D getPose() const;
+
+private:
+    Eigen::Vector3f state; // [x, y, theta]
+    Eigen::Matrix3f P;     // Covariance
+    float dt;
+
+    Eigen::Matrix3f Q; // Process noise
+    Eigen::Matrix3f R; // Measurement noise (yaw)
+
+    float normalizeAngle(float angle);
+};
