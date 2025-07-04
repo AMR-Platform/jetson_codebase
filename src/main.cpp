4ddd9938a6a452edf18dc80ab9dc709f01d9deba
Savindu Dilshan <savindudilshan124@gmail.com>
2025-07-04 07:51:01 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "lidar_handler.hpp"
 #include <iostream>
 #include <thread>
+#include <chrono>
 
 int main() {
     LidarHandler handler;
@@ -8,12 +9,16 @@ int main() {
     while (true) {
         handler.poll();
         auto scan = handler.getLatestScan();
-        
+
         if (!scan.empty()) {
-            std::cout << "Scan with " << scan.size() << " points received.\n";
+            std::cout << "Received " << scan.size() << " points.\n";
+            for (size_t i = 0; i < std::min(scan.size(), size_t(5)); ++i) {
+                std::cout << "  Point " << i << ": Azimuth = " << scan[i].azimuth
+                          << "°, Distance = " << scan[i].distance << " m\n";
+            }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     return 0;
