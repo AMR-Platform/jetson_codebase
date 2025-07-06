c5d2c9f8a71eb9e4026caa7fe7311d172797ad5b
KiranGunathilaka <kirangunathilaka@gmail.com>
2025-07-06 15:06:40 +0530

    cmake issue fixed

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <boost/filesystem.hpp>
 #include <chrono>
 
-<<<<<<< HEAD
 namespace fs = boost::filesystem;
 
 /* ---------- misc helpers ------------------------------------------------ */
